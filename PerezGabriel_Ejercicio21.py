68f2c4b96f5e92d7a5c31977cb8f67c68b7d8516
Default user <ga.perezg1@uniandes.edu.co>
2019-10-22 18:31:59 +0000

    Codigo y grafica actualizadas

--- PerezGabriel_Ejercicio21.py
+++ PerezGabriel_Ejercicio21.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-np.pi,np.pi,100)
-y = np.sin(x)
+y = np.cos(x)
 
 plt.plot(x,y)
 plt.xlabel('x')
-plt.ylabel('y = sin(x)')
+plt.ylabel('y = cos(x)')
 plt.grid()
 plt.savefig('grafica.png')
